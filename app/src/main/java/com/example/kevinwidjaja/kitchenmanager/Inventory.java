d153dce7ed4dc594bf374b076dea82508da71e3c
widkvn <kevwid89@gmail.com>
2015-03-31 21:34:01 -0700

    inventory model class fix: constructor missing name assignment

--- app/src/main/java/com/example/kevinwidjaja/kitchenmanager/Inventory.java
+++ app/src/main/java/com/example/kevinwidjaja/kitchenmanager/Inventory.java
@@ -17,6 +17,7 @@ public class Inventory {
 
     public Inventory(int id, String name, int unit_id, int quantity){
         this.id = id;
+        this.name = name;
         this.unit_id = unit_id;
         this.quantity = quantity;
     }
